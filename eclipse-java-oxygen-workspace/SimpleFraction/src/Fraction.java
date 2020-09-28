a532faf86cc4b0dfcfb4c54f5fadf78d668f0b6d
uvsq21801213 <quentin.thierry@ens.uvsq.fr>
2020-09-28 15:18:45 +0200

    ajout de toString

--- eclipse-java-oxygen-workspace/SimpleFraction/src/Fraction.java
+++ eclipse-java-oxygen-workspace/SimpleFraction/src/Fraction.java
@@ -1,4 +1,17 @@
 
 public class Fraction {
-
+	
+	float numerateur;
+	float denominateur;
+	
+	Fraction(float a, float b){
+		numerateur = a;
+		denominateur = b;
+	}
+	
+	
+	public String toString() {
+		
+		return this.numerateur + "/" + this.denominateur;
+	}
 }
